8b45312e82ca983e7e9827b315529e1ac149cf21
Felipe Morales <hel.sheep@gmail.com>
2014-05-04 11:41:50 -0400

    Make async.py use --remote-send on Windows.
    --remote-expr seems to fail.
    
    Re: issue #29.

--- pythonx/pantondoc/async.py
+++ pythonx/pantondoc/async.py
@@ -18,13 +18,18 @@ if __name__ == "__main__":
 
     # once it's done, we call back the server that called us
     # to notify pandoc's execution
+    func_call = "pantondoc#command#PandocAsyncCallback("+should_open+","+returncode+")"
+
     if sys.platform.startswith("win"):
         # TODO: make this hardcoded value dynamic or configurable
-        command = ["C:\Program Files\Vim\\vim74\vim.exe"]
+        command = ["C:\Program Files\Vim\\vim74\\vim.exe"]
     else:
         command = ["vim"]
-    args = " ".join(["--servername", servername,  \
-                     "--remote-expr \"pantondoc#command#PandocAsyncCallback(" + should_open + ", " + returncode + ")\""])
-    command.extend(shlex.split(args))
+    command.extend(["--servername", servername])
+    # windows requires the callback name to be sent instead of being eval'ed,
+    # for some reason. note this is more fragile.
+    if sys.platform.startswith("win"):
+        command.extend(["--remote-send", "<ESC>:call " + func_call + "<CR>"])
+    else:
+        command.extend(["--remote-expr", func_call])
     Popen(command)
-
