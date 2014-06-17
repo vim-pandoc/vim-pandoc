932f19c1eab65d3084a6730ef6a3628a386543a4
Timothée Poisot <t.poisot@gmail.com>
2014-06-17 18:17:59 -0400

    More general form

--- pythonx/pantondoc/bib.py
+++ pythonx/pantondoc/bib.py
@@ -8,7 +8,7 @@ import subprocess as sp
 
 def make_title_ascii(title):
     import unicodedata
-    if type(title) != ascii :
+    if type(title) != str :
         title = unicodedata.normalize('NFKD', title).encode('ascii', 'ignore')
     else :
         title = str(title)
