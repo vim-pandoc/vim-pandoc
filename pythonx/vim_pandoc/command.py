vim-pandoc/vim-pandoc

commit 7ffb061d283e9cbc20bf47f008370e1e6a135686

pandoc/command: add support for python3 to the :Pandoc command

pythonx/vim_pandoc/command.py
@@ -62,6 +62,19 @@ markdown_extensions = [
         "compact_definition_lists"
         ]
 
+def get_raw_pandoc_data(pattern):
+    data = str(Popen(["pandoc", "--help"], stdout=PIPE).communicate()[0])
+    return re.search(pattern, data, re.DOTALL).group(1)
+
+def wrap_formats(fn):
+        # pandoc's output changes depending on platform
+        if sys.platform == "win32":
+            splitter = '\r\n'
+        else:
+            splitter = '\n'
+        data = fn()
+        return lambda: re.findall('(\w+\**)[,'+splitter+']', data)
+
 class PandocHelpParser(object):
     def __init__(self):
         self._help_data = Popen(["pandoc", "--help"], stdout=PIPE).communicate()[0]
@@ -70,17 +83,17 @@ class PandocHelpParser(object):
 
     @staticmethod
     def get_longopts():
-        data = Popen(["pandoc", "--help"], stdout=PIPE).communicate()[0]
+        data = str(Popen(["pandoc", "--help"], stdout=PIPE).communicate()[0])
         return map(lambda i: i.replace("--", ""), \
                    filter(lambda i: i not in ("--version", "--help", "--to", "--write"), \
                           [ i.group() for i in re.finditer("-(-\w+)+=?", data)]))
 
     @staticmethod
     def get_shortopts():
-        data = Popen(["pandoc", "--help"], stdout=PIPE).communicate()[0]
-        no_args = map(lambda i: i.replace("-", "").strip(), \
+        data = str(Popen(["pandoc", "--help"], stdout=PIPE).communicate()[0])
+        no_args = list(map(lambda i: i.replace("-", "").strip(), \
                       filter(lambda i: i not in ("-v ", "-h "), \
-                             [i.group() for i in re.finditer("-\w\s(?!\w+)", data)]))
+                             [i.group() for i in re.finditer("-\w\s(?!\w+)", data)])))
 
         # -m doesn't comply with the format of the other short options in pandoc 1.12
         # if you need to pass an URL, use the long versions
@@ -94,22 +107,14 @@ class PandocHelpParser(object):
         return "".join(no_args) + "".join(map(lambda i: i + ":", args))
 
     @staticmethod
-    def _get_formats():
-        # pandoc's output changes depending on platform
-        if sys.platform == "win32":
-            splitter = '\r\n'
-        else:
-            splitter = '\n'
-        data = Popen(["pandoc", "--help"], stdout=PIPE).communicate()[0]
-        return " ".join(re.findall('(\w+\**[,'+splitter+'])+', data)).split(splitter[0])[:2]
-
-    @staticmethod
+    @wrap_formats
     def _get_input_formats():
-        return map(lambda i: i.strip(), PandocHelpParser._get_formats()[0].split(", "))
+        return get_raw_pandoc_data("Input formats:(.*)Output formats")
 
     @staticmethod
+    @wrap_formats
     def _get_output_formats():
-        return map(lambda i: i.strip().replace("*", ""), PandocHelpParser._get_formats()[1].split(", "))
+        return get_raw_pandoc_data("Output formats:(.*)\[\*for pdf")
 
     @staticmethod
     def get_output_formats_table():
@@ -206,7 +211,7 @@ class PandocCommand(object):
 
         output_format = c_args.pop(0) if len(c_args) > 0 and self.opts.in_allowed_formats(c_args[0]) else "html"
         output_format_arg = "-t " + output_format if output_format != "pdf" else ""
-        
+
         def no_extensions(fmt):
             return re.split("[-+]", fmt)[0]
 

autoload/pandoc/command.vim
@@ -30,9 +30,16 @@ function! pandoc#command#Init()
     endif
 
     " create :Pandoc {{{2
-    if has("python") || has("python/dyn")
+    if has("python") || has("python/dyn") || has("python3") || has("python3/dyn")
+        if has("python") || has("python/dyn")
+            let s:python = "py "
+            let s:pyeval = "pyeval"
+        else
+            let s:python = "py3 "
+            let s:pyeval = "py3eval"
+        endif
         " let's make sure it gets loaded
-        py import vim
+        exe s:python ."import vim"
         command! -buffer -bang -nargs=? -complete=customlist,pandoc#command#PandocComplete
                     \ Pandoc call pandoc#command#Pandoc("<args>", "<bang>")
     endif "}}}2
@@ -50,25 +57,25 @@ endfunction
 " args: arguments to pass pandoc
 " bang: should we open the created file afterwards?
 function! pandoc#command#Pandoc(args, bang)
-    if has("python") || has("python/dyn")
-        py from vim_pandoc.command import pandoc
+    if has("python") || has("python/dyn") || has("python3") || has("python3/dyn")
+        exe s:python ."from vim_pandoc.command import pandoc"
         let templatized_args = substitute(a:args, '#\(\S\+\)',
                     \'\=pandoc#command#GetTemplate(submatch(1))', 'g')
-        py pandoc(vim.eval("templatized_args"), vim.eval("a:bang") != '')
+        exe s:python ."pandoc(vim.eval('templatized_args'), vim.eval('a:bang') != '')"
     endif
 endfunction
 
 " PandocComplete(a, c, pos): the Pandoc command argument completion func, requires python support {{{2
 function! pandoc#command#PandocComplete(a, c, pos)
-    if has("python")
-        py from vim_pandoc.command import PandocHelpParser
+    if has("python") || has("python/dyn") || has("python3") || has("python3/dyn")
+        exe s:python ."from vim_pandoc.command import PandocHelpParser"
         let cmd_args = split(a:c, " ", 1)[1:]
-        if len(cmd_args) == 1 && (cmd_args[0] == '' || pyeval("vim.eval('cmd_args[0]').startswith(vim.eval('a:a'))"))
-            return pyeval("filter(lambda i: i.startswith(vim.eval('a:a')), sorted(PandocHelpParser.get_output_formats_table().keys()))")
+        if len(cmd_args) == 1 && (cmd_args[0] == '' || eval(s:pyeval .'("'."vim.eval('cmd_args[0]').startswith(vim.eval('a:a'))".'")'))
+            exe "return ".s:pyeval.'("'."filter(lambda i: i.startswith(vim.eval('a:a')), sorted(PandocHelpParser.get_output_formats_table().keys()))".'")'
         endif
         if len(cmd_args) >= 2
-            let long_opts = pyeval("['--' + i for i in filter(lambda i: i.startswith(vim.eval('a:a[2:]')), PandocHelpParser.get_longopts())]")
-            let short_opts = pyeval("['-' + i for i in filter(lambda i: i.startswith(vim.eval('a:a[1:]')), PandocHelpParser.get_shortopts())]")
+            let long_opts = eval(s:pyeval . '("'. "['--' + i for i in filter(lambda i: i.startswith(vim.eval('a:a[2:]')), PandocHelpParser.get_longopts())]".'")')
+            let short_opts = eval(s:pyeval . '("'. "['-' + i for i in filter(lambda i: i.startswith(vim.eval('a:a[1:]')), PandocHelpParser.get_shortopts())]". '")')
             return filter(uniq(extend(sort(short_opts), sort(long_opts))), 'v:val != "-:"')
         endif
     endif
@@ -91,17 +98,13 @@ endfunction
 " should_open: should we open the cretaed file?
 " returncode: the returncode value pandoc gave
 function! pandoc#command#PandocAsyncCallback(should_open, returncode)
-    if has("python")
-        py from vim_pandoc.command import pandoc
-        py pandoc.on_done(vim.eval("a:should_open") == '1', vim.eval("a:returncode"))
-    endif
+    exe s:python ."from vim_pandoc.command import pandoc"
+    exe s:python ."pandoc.on_done(vim.eval('a:should_open') == '1', vim.eval('a:returncode'))"
 endfunction
 
 function! pandoc#command#JobHandler(id, data, event)
-    if has("python")
-        py from vim_pandoc.command import pandoc
-        py pandoc.on_done(vim.eval("self.should_open") == '1', vim.eval("a:data"))
-    endif
+    exe s:python ."from vim_pandoc.command import pandoc"
+    exe s:python ."pandoc.on_done(vim.eval('self.should_open') == '1', vim.eval('a:data'))"
 endfunction
 
 " Command template functions {{{1

commit f17b67be6048b73333d556da1fb051cdbaeec9e3

Fixed issue #133, caused by a regression in 7ffb061d283e9cbc20bf47f008370e1e6a135686

pythonx/vim_pandoc/command.py
@@ -114,7 +114,7 @@ class PandocHelpParser(object):
     @staticmethod
     @wrap_formats
     def _get_output_formats():
-        return get_raw_pandoc_data("Output formats:(.*)\[\*for pdf")
+        return get_raw_pandoc_data("Output formats:(.*)\[\*\*for pdf")
 
     @staticmethod
     def get_output_formats_table():