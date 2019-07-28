vim-pandoc/vim-pandoc

commit d6d54d9f3782e1d1dbc66aef9b3cd47e27d58b8e

reset options after switching filetype (to, say, markdown) (#322)

ftplugin/pandoc.vim
@@ -33,4 +33,10 @@ endif
 setlocal formatlistpat=\\C^\\s*[\\[({]\\\?\\([0-9]\\+\\\|[iIvVxXlLcCdDmM]\\+\\\|[a-zA-Z]\\)[\\]:.)}]\\s\\+\\\|^\\s*[-+o*]\\s\\+
 setlocal formatoptions+=n
 
+let b:undo_ftplugin = "setlocal formatoptions< formatlistpat<"
+                \ . "| unlet b:match_pairs b:match_words"
+if exists('g:pandoc#formatting#equalprg') && !empty(g:pandoc#formatting#equalprg)
+    let b:undo_ftplugin .= "| setlocal equalprg<"
+endif
+
 let b:pandoc_loaded = 1

commit e8ad9e3e2a9219905e4e6638b663e5e51dbca743

Hotfix to avoid error condition introduced in d6d54d9, see #322

ftplugin/pandoc.vim
@@ -34,7 +34,7 @@ setlocal formatlistpat=\\C^\\s*[\\[({]\\\?\\([0-9]\\+\\\|[iIvVxXlLcCdDmM]\\+\\\|
 setlocal formatoptions+=n
 
 let b:undo_ftplugin = "setlocal formatoptions< formatlistpat<"
-                \ . "| unlet b:match_pairs b:match_words"
+                \ . "| unlet! b:match_pairs b:match_words"
 if exists('g:pandoc#formatting#equalprg') && !empty(g:pandoc#formatting#equalprg)
     let b:undo_ftplugin .= "| setlocal equalprg<"
 endif